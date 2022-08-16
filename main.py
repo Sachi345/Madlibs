ccb73ce69eb95f8b7e663988643c88604199bda8
sachi345 <cmaheeshi88@gmail.com>
2022-08-16 11:54:19 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+exclamation = input("Enter an exclamation: ")
+adverb = input("Enter an adverb : ")
+noun = input("Enter a noun : ")
+adjective = input("Enter an adjective : ")
+
+sentence = (
+    f'{exclamation},! he said {adverb} as he jumped into his convertible '
+    f'{noun} and drove off with his {adjective} wife.')
+
+print(sentence)
